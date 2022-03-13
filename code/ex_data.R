8294470a7c2e81b6bd4621b1d22bf14294a6a40a
eunjoo1001 <leeeunjoo1001@hanmail.net>
2022-03-13 23:55:05 +0900

    new commit

--- code/ex_data.R
+++ code/ex_data.R
@@ -1,1 +1,22 @@
+# Set up
+install.packages("data.table")
+install.packages("curl")
+
+library(data.table)
+library(curl)
+
+# Load file
+url <- "https://raw.githubusercontent.com/jinseob2kim/lecture-snuhlab/master/data/example_g1e.csv"
+df <- read.table(url, header=T)
+dt <- fread(url, head=T)
+print(class(df))
+print(class(dt))
+dt
+
+
+
+
+
+
+
 
