555d43801729283e03a5d8c9ef31c461d681803b
eunjoo1001 <leeeunjoo1001@hanmail.net>
2022-03-13 22:39:48 +0900

    new commit

--- code/install.R
+++ code/install.R
@@ -1,1 +1,25 @@
 install.packages(c("data.table", "magrittr", "fst", "ggplot2", "ggpubr", "officer", "rvg", "tableone", "gtsummary", "MatchIt", "twang", "usethis", "gitcreds"))
+
+
+## setting: permanent storage
+credentials::credential_helper_set("manager")  
+
+## create a personal access token for authentication:
+usethis::create_github_token()
+
+## copy and paste your token and press enter
+credentials::set_github_pat()
+
+#### 4. Restart R! ###########################################################
+
+# ----------------------------------------------------------------------------
+
+#### 5. Verify settings ######################################################
+
+usethis::git_sitrep()
+
+## Your username and email should be stated correctly in the output. 
+## Also, the report shoud cotain something like:
+## 'Personal access token: '<found in env var>''
+
+# ----------------------------------------------------------------------------
